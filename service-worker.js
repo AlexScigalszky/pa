b70af47a93d76543796c10febe004e8a30785f17
Alex P. Scigalszky <alexpablo.90@gmail.com>
2021-12-01 09:31:51 -0300

    deploy

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pa/precache-manifest.6ccdbbe6e1b99406256a481e9f4a229b.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "palabras-aleatorias-pwa"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
