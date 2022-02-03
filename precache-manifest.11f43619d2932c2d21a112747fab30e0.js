260a6e9cb0b047e8b4c1f5213e181faf3902d549
AlexScigalszky <alexpablo.90@gmail.com>
2022-02-03 19:31:07 +0000

    See https://github.com/AlexScigalszky/palabras-aleatorias-pwa/commit/9ad020f75ba8d26ed0bbff5e0b7dc318f5c9aafb from refs/heads/main

---
+++ precache-manifest.11f43619d2932c2d21a112747fab30e0.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00f0bbf70e33f9afaced3fee1b6705f",
+    "url": "palabrasaleatorias.ar/audio/beep-05.mp3"
+  },
+  {
+    "revision": "b76b0645d3ba50d44def821b619ab0b6",
+    "url": "palabrasaleatorias.ar/audio/beep-08b.mp3"
+  },
+  {
+    "revision": "c9d741797ded46c268471ab10b734652",
+    "url": "palabrasaleatorias.ar/audio/clock.wav"
+  },
+  {
+    "revision": "5e2e23974a1a7d60c7abf125061f80cc",
+    "url": "palabrasaleatorias.ar/audio/tick.mp3"
+  },
+  {
+    "revision": "ef79288dd92db599d5feb1444f2fe5b5",
+    "url": "palabrasaleatorias.ar/audio/tock.mp3"
+  },
+  {
+    "revision": "eb39f45946760fb52fa3",
+    "url": "palabrasaleatorias.ar/css/about.7867575f.css"
+  },
+  {
+    "revision": "b3311149c9508d81c919",
+    "url": "palabrasaleatorias.ar/css/app.03ac8547.css"
+  },
+  {
+    "revision": "6528c0ffa0d2518120a0",
+    "url": "palabrasaleatorias.ar/css/chunk-vendors.bddfda6d.css"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "palabrasaleatorias.ar/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "palabrasaleatorias.ar/fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "palabrasaleatorias.ar/fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "palabrasaleatorias.ar/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "palabrasaleatorias.ar/fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "palabrasaleatorias.ar/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "palabrasaleatorias.ar/fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "palabrasaleatorias.ar/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "palabrasaleatorias.ar/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "palabrasaleatorias.ar/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "palabrasaleatorias.ar/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "palabrasaleatorias.ar/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "c270b1734890e43c1782b0c8eded3f43",
+    "url": "palabrasaleatorias.ar/img/alarm.png"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "palabrasaleatorias.ar/img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "palabrasaleatorias.ar/img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "palabrasaleatorias.ar/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "0c41f5ce58b2a2d03e1af386d5185c52",
+    "url": "palabrasaleatorias.ar/img/icon.jpg"
+  },
+  {
+    "revision": "fbc2cb513ff91dd032f18aaa55f6b7c7",
+    "url": "palabrasaleatorias.ar/img/icon.png"
+  },
+  {
+    "revision": "fb7d8c24f61a00240ae33a5267734438",
+    "url": "palabrasaleatorias.ar/img/palabras_aleatorias_share.png"
+  },
+  {
+    "revision": "e535ce83da20a4b7719ca3d45195ebd5",
+    "url": "palabrasaleatorias.ar/img/shapes.svg"
+  },
+  {
+    "revision": "8fa2d9075c61f2dcd9c86515622d4375",
+    "url": "palabrasaleatorias.ar/img/splash.png"
+  },
+  {
+    "revision": "e2570db9213b805b3b03aae19bb1a62f",
+    "url": "palabrasaleatorias.ar/index.html"
+  },
+  {
+    "revision": "eb39f45946760fb52fa3",
+    "url": "palabrasaleatorias.ar/js/about.a117606a.js"
+  },
+  {
+    "revision": "b3311149c9508d81c919",
+    "url": "palabrasaleatorias.ar/js/app.1e396fa6.js"
+  },
+  {
+    "revision": "6528c0ffa0d2518120a0",
+    "url": "palabrasaleatorias.ar/js/chunk-vendors.a9605552.js"
+  },
+  {
+    "revision": "dce2e029f0f4a581439c",
+    "url": "palabrasaleatorias.ar/js/comments.86edda7d.js"
+  },
+  {
+    "revision": "9966c07f811fdf510feb",
+    "url": "palabrasaleatorias.ar/js/saved.55200871.js"
+  },
+  {
+    "revision": "e37ee1bb4a62d1fc9ab6",
+    "url": "palabrasaleatorias.ar/js/setttings.53861086.js"
+  },
+  {
+    "revision": "0d43e0d46dc83e2a5253",
+    "url": "palabrasaleatorias.ar/js/show-comments.e0d1eb4b.js"
+  },
+  {
+    "revision": "631d87f4fd1e9b74561f",
+    "url": "palabrasaleatorias.ar/js/words-group.261518e5.js"
+  },
+  {
+    "revision": "8183cc64994391e35508e330ff406730",
+    "url": "palabrasaleatorias.ar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "palabrasaleatorias.ar/robots.txt"
+  }
+]);
